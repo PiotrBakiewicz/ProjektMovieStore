b369e7977f912970bebbfd6a8e3c301f3f9299c1
PiotrBakiewicz <mojave@onet.eu>
2018-07-04 16:03:59 +0200

    Dane klientów

--- src/Customers.java
+++ src/Customers.java
@@ -1,3 +1,42 @@
 public class Customers {
+    private String name;
+    private String adress;
+    private String sex;
+    private String email;
+    private int phone;
+    private int age;
+
+    Customers(String name,
+            String adress,
+            String sex,
+            String email,
+            int phone,
+            int age) {
+        this.name = name;
+        this.adress = adress;
+        this.sex = sex;
+        this.email = email;
+        this.phone = phone;
+        this.age = age;
+
+    }
+
+
+
+
+
+
+    public void showCustomers() {
+        System.out.println("Nazwisko: " + name);
+        System.out.println("Adres: " + adress);
+        System.out.println("Płeć: " + sex);
+        System.out.println("E-mail: " + email);
+        System.out.println("Telefon: " + phone);
+        System.out.println("Wiek: " + age);
+    }
+
+    public boolean sprawdzNazwisko(String tytul) {
+        return this.adress == name;
+    }
+
 }
-// tu Piotr Bedzię pracował
